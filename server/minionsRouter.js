062a99ce81cdf06bdeee0e4043226c59623627fa
dlord-20 <dthomaslord@gmail.com>
2022-11-08 19:03:26 +0300

    Updated routes for minions router. These should be fully working now

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -13,24 +13,44 @@ const {
 
 minionsRouter.get('/', (req, res, next) => {
     const allMinions = getAllFromDatabase('minions');
-    console.log(allMinions);
     res.send(allMinions);
 });
 
 minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', res.body);
+    res.status(202).send(newMinion);
+});
+
+minionsRouter.param('/:minionId', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+
+    if(minion) {
+        req.minion = minion;
+        req.id = id;
+        next();
+    } else {
+        res.status(404).send();
+    }
 
 });
 
 minionsRouter.get('/:minionId', (req, res, next) => {
-
+    res.status(200).send(req.minion);
 });
 
 minionsRouter.put('/:minionId', (req, res, next) => {
-
+    const updatedMinion = updateInstanceInDatabase('minions', req.body);
+    res.status(200).send(updatedMinion);
 });
 
 minionsRouter.delete('/:minionId', (req, res, next) => {
-
+    const deleted = deleteFromDatabasebyId('minions', req.id);
+    if(deleted) {
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
 });
 
 module.exports = minionsRouter;

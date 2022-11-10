358646fa62fa80fa282cc37e097d65f1e7558935
dlord-20 <dthomaslord@gmail.com>
2022-11-10 10:18:42 +0300

    All finished and will restart the project without help on Sunday

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -31,6 +31,7 @@ minionsRouter.param('minionId', (req, res, next, id) => {
     }
 
 });
+//Im coming back to this and will restart it next week
 
 minionsRouter.get('/:minionId', (req, res, next) => {
     res.status(200).send(req.minion);
